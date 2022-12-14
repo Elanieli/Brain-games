7b1adeb64da8efe7fc089d505eaa8800f9c546eb
Maria Oleynikova <oleyn11@mail.ru>
2022-12-14 13:47:24 +0700

    change src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,18 +1,27 @@
-import commonLogic from '../index.js';
+import readlineSync from 'readline-sync';
 
 const playGame = () => {
-  const taskDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+  console.log('Welcome to the Brain Games!');
 
-  const taskEven = () => {
-    const question = Math.floor(Math.random() * (100 - 1)) + 1;
-    let result = '';
-    if (question % 2 === 0) {
-      result = 'yes';
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    const num = Math.floor(Math.random() * (100 - 1)) + 1;
+    console.log('Question: ', String(num));
+    const userAnswer = readlineSync.question('Your answer: ');
+    const var1 = (num % 2 === 0 && userAnswer === 'yes');
+    const var2 = (num % 2 !== 0 && userAnswer === 'no');
+    const oppositeAnswer = (userAnswer === 'yes' ? 'no' : 'yes');
+
+    if (var1 === true || var2 === true) {
+      console.log('Correct!');
     } else {
-      result = 'no';
+      const wrongAnswer = console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${oppositeAnswer}'. \n Let's try again, ${name}!`);
+      return wrongAnswer;
     }
-    return [String(question), result];
-  };
-  commonLogic(taskDescription, taskEven);
+  }
+  return console.log(`Congratulations, ${name}!`);
 };
 export default playGame;

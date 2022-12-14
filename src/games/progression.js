19091200dc43d9d06fd212506c9822b46bb1319b
Maria Oleynikova <oleyn11@mail.ru>
2022-12-14 14:42:27 +0700

    change src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,18 +5,18 @@ const progression = () => {
 
   const taskProgression = () => {
     const randomStep = Math.ceil(Math.random() * 5);
-    const a = Math.ceil(Math.random() * 10);
-    const b = Math.ceil(Math.random() * 15);
+    const a = Math.ceil(Math.random() * 15);
+    const b = Math.ceil(Math.random() * 20);
 
     const question = [];
 
-    for (let i = Math.min(a, b); i < Math.max(a, b) * 10 && question.length < 10; i += randomStep) {
-      question.push(i);
+    for (let i = Math.min(a, b); i < Math.max(a, b) * 10 && question.length < 11; i += randomStep) {
+      question.push(String(i));
     }
     const randomQuestionIndex = Math.floor(Math.random() * question.length);
     const result = question[randomQuestionIndex];
     question[randomQuestionIndex] = '..';
-    return [String(question.join(' ')), String(result)];
+    return [question.join(' '), String(result)];
   };
   commonLogic(taskDescription, taskProgression);
 };

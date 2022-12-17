bd27aca8f7755787f3f1f2d538bbc1d1089b2ba3
Maria Oleynikova <oleyn11@mail.ru>
2022-12-17 20:00:54 +0700

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 import readlineSync from 'readline-sync';
 
-const commonLogic = (taskDecription, task) => {
+const runEngine = (taskDecription, task) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(taskDecription);
 
-  for (let i = 0; i < 3; i += 1) {
-    const taskAndResult = task();
-    console.log('Question:', taskAndResult[0]);
+  const finish = 3;
+
+  for (let i = 0; i < finish; i += 1) {
+    const [question, result] = task();
+    console.log('Question:', question);
     const userAnswer = readlineSync.question('Your answer: ');
 
-    if (userAnswer === taskAndResult[1]) {
+    if (userAnswer === result) {
       console.log('Correct!');
     } else {
-      const wrongAnswer = console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${taskAndResult[1]}'. \n Let's try again, ${userName}!`);
-      return wrongAnswer;
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${result}'. \n Let's try again, ${userName}!`);
+      return;
     }
   }
-  return console.log(`Congratulations, ${userName}!`);
+  console.log(`Congratulations, ${userName}!`);
 };
-export default commonLogic;
+export default runEngine;

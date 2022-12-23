1428a7b4ff5d2a845dfd4a4b6aa5c980312ee5e6
Maria Oleynikova <oleyn11@mail.ru>
2022-12-23 15:51:09 +0700

    change src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,24 +1,25 @@
 import runEngine from '../index.js';
+import getRandomInRange from '../utils.js';
 
-const progression = () => {
-  const taskDescription = 'What number is missing in the progression?';
+const taskDescription = 'What number is missing in the progression?';
 
-  const taskProgression = () => {
-    const randomStep = Math.ceil(Math.random() * 5);
-    const a = Math.ceil(Math.random() * 15);
-    const b = Math.ceil(Math.random() * 20);
-
-    const question = [];
-
-    for (let i = Math.min(a, b); i < Math.max(a, b) * 10 && question.length < 11; i += randomStep) {
-      question.push(String(i));
-    }
-    const randomQuestionIndex = Math.floor(Math.random() * question.length);
-    const result = question[randomQuestionIndex];
-    question[randomQuestionIndex] = '..';
-    return [question.join(' '), String(result)];
-  };
-  runEngine(taskDescription, taskProgression);
+const calc = (a, b, randomStep) => {
+  const question = [];
+  for (let i = Math.min(a, b); i < Math.max(a, b) * 10 && question.length < 11; i += randomStep) {
+    question.push(String(i));
+  }
+  return question;
 };
 
-export default progression;
+const generateRound = () => {
+  const randomStep = getRandomInRange(1, 5);
+  const firstNum = getRandomInRange(1, 15);
+  const secondNum = getRandomInRange(1, 10);
+  const question = calc(firstNum, secondNum, randomStep);
+  const randomQuestionIndex = getRandomInRange(0, question.length);
+  const answer = String(question[randomQuestionIndex]);
+  question[randomQuestionIndex] = '..';
+  return [question.join(' '), answer];
+};
+
+export default () => runEngine(taskDescription, generateRound);

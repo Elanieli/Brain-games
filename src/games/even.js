fa16f6e7b3b2aec63cfabbd86e009bd34bc30688
Maria Oleynikova <oleyn11@mail.ru>
2022-12-23 15:47:30 +0700

    change src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,18 +1,14 @@
 import runEngine from '../index.js';
+import getRandomInRange from '../utils.js';
 
-const playGame = () => {
-  const taskDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+const taskDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-  const taskEven = () => {
-    const question = Math.floor(Math.random() * (100 - 1)) + 1;
-    let result = '';
-    if (question % 2 === 0) {
-      result = 'yes';
-    } else {
-      result = 'no';
-    }
-    return [String(question), result];
-  };
-  runEngine(taskDescription, taskEven);
+const isEven = (question) => String(question % 2 === 0 ? 'yes' : 'no');
+
+const generateRound = () => {
+  const question = getRandomInRange(1, 100);
+  const result = isEven(question);
+  return [question, result];
 };
-export default playGame;
+
+export default () => runEngine(taskDescription, generateRound);

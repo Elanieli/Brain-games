5371746a78a0ba8d767a840f2f7f2bf1a2c844fb
Maria Oleynikova <oleyn11@mail.ru>
2022-12-23 15:51:41 +0700

    change src/index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import readlineSync from 'readline-sync';
 
-const runEngine = (taskDecription, task) => {
+const roundsCount = 3;
+
+const runEngine = (taskDecription, generateRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(taskDecription);
 
-  const finish = 3;
-
-  for (let i = 0; i < finish; i += 1) {
-    const [question, result] = task();
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [question, answer] = generateRound();
     console.log('Question:', question);
     const userAnswer = readlineSync.question('Your answer: ');
 
-    if (userAnswer === result) {
+    if (userAnswer === answer) {
       console.log('Correct!');
     } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${result}'. \n Let's try again, ${userName}!`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'. \n Let's try again, ${userName}!`);
       return;
     }
   }
   console.log(`Congratulations, ${userName}!`);
 };
+
 export default runEngine;

f7797328aeff23ebbfd70eaac71ada5af840c46a
Maria Oleynikova <oleyn11@mail.ru>
2022-12-28 12:22:12 +0700

    change src/games/gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,35 +3,18 @@ import getRandomInRange from '../utils.js';
 
 const taskDecription = 'Find the greatest common divisor of given numbers.';
 
-const calc = (firstNum, secondNum) => {
-  let result;
-  let num1 = firstNum;
-  let num2 = secondNum;
-
-  while (num1 !== num2) {
-    if (num1 === 0) {
-      result = secondNum;
-      break;
-    }
-    if (num2 === 0) {
-      result = num1;
-      break;
-    }
-    if (num1 > num2) {
-      num1 -= num2;
-    } else {
-      num2 -= num1;
-    }
-    result = num1;
+const getGcd = (num1, num2) => {
+  if (num1 === 0) {
+    return num2;
   }
-  return result;
+  return getGcd(num2 % num1, num1);
 };
 
 const generateRound = () => {
   const firstNum = getRandomInRange(1, 40);
   const secondNum = getRandomInRange(1, 35);
   const question = `${firstNum} ${secondNum}`;
-  const answer = String(calc(firstNum, secondNum));
+  const answer = String(getGcd(firstNum, secondNum));
   return [question, answer];
 };
 
